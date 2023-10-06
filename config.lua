63c20c09f09f4fc1be6e1d88211765b9d5140578
Arya Ngurah Intaran <aryaintaran24@gmail.com>
2023-10-07 02:45:39 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+QB = {}
+
+QB.Spawns = {
+    ["harbour"] = {
+        coords = vector4(-732.45, -1304.6, 5.05, 20.72),
+        location = "harbour",
+        label = "Pelabuhan",
+    },
+
+    ["airport"] = {
+        coords = vector4(-1041.67, -2745.64, 21.36, 334.23),
+        location = "airport",
+        label = "Bandara",
+    },
+}
